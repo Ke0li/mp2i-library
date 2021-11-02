4b6929972348081330119ef4ec2cbdba35bc28db
Eloi <90042111+Ke0li@users.noreply.github.com>
2021-11-02 18:24:43 +0100

    Update algo quicksort.ml

--- OCAML/Tri fusion ; tri rapide/tri rapide/algo quicksort.ml
+++ OCAML/Tri fusion ; tri rapide/tri rapide/algo quicksort.ml
@@ -1,3 +1,17 @@
+
+(*renvoie une liste contenant les elements de l1 puis les elements de l2*)
+let rec _concat l1 l2 = match l1 with
+    | [] -> l2
+    | e::q -> e::(_concat q l2);;
+
+(*decoupe la liste en deux , d'un cote les elements plus grand que p et les autres*)
+let rec _partition p l = match l with
+    | [] -> ([], [])
+    | e::q -> let l1, l2 = (_partition p q) in
+                if e < p then ((e::l1), l2)
+                else (l1, (e::l2));;
+
+
 (*la fonction quicksort l utilise l'algorithme quicksort pour trier la liste l *)
 
 let rec quicksort l = match l with
