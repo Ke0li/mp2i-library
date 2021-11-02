6c8ab2bd66924cf4928ce288c18feee29d9ac278
Eloi <90042111+Ke0li@users.noreply.github.com>
2021-11-02 18:00:38 +0100

    Update algo fusion.ml

--- OCAML/Tri fusion ; tri rapide/tri fusion/algo fusion.ml
+++ OCAML/Tri fusion ; tri rapide/tri fusion/algo fusion.ml
@@ -1,1 +1,22 @@
+(*algo tri fusion*)
 
+(*algo pour diviser une liste en deux*)
+let rec split = function
+  | [] -> [], []
+  | [e] -> [e], []
+  | e1::e2::q -> let q1, q2 = split q in
+  e1::q1, e2::q2
+
+(*algo pour fusionner deux listes triées*)
+let rec fusion l1 l2 = match l1, l2 with
+  | [], _ -> l2
+  | _, [] -> l1
+  | e1::q1, e2::q2 when e1 < e2 -> e1::fusion q1 l2
+  | e1::q1, e2::q2 -> e2::fusion l1 q2
+
+(*algo tri fusion*)
+let rec tri = function
+  | [] -> []
+  | [e] -> [e] (* tri ne termine pas sans ce cas *)
+  | l -> let l1, l2 = split l in
+fusion (tri l1) (tri l2);;
