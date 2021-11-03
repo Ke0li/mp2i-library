1a6a68496846ab531db50e083cd407d2c645c76b
Eloi <90042111+Ke0li@users.noreply.github.com>
2021-11-03 18:46:14 +0100

    Update and rename OCAML/recherche/recherche.ml to OCAML/recherche/array/search_array.ml

--- OCAML/recherche/array/search_array.ml
+++ OCAML/recherche/array/search_array.ml
@@ -22,3 +22,27 @@ let tricho t e =
         else if e < t.(m2) then aux (m1 + 1) (m2 - 1)
         else aux (m2 + 1) j in
   aux 0 (Array.length t - 1)
+  
+  
+(* renvoie un maximum local*)
+let max_local t = 
+    let n = Array.length t in
+    let rec aux i j =
+        let m = (i + j)/2 in
+        if (m = 0 || t.(m) >= t.(m-1)) && (m = n - 1 || t.(m) >= t.(m+1))
+        then m
+        else if t.(m) < t.(m - 1) then aux i (m - 1)
+        else aux (m + 1) j in
+    aux 0 (n + 1);;
+
+
+(* recherche de la tranche maximum dans un tableau *)
+let tranche_max t =
+    let m = ref t.(0) in
+    let m_cur = ref t.(0) in
+    for i = 1 to Array.length t - 1 do
+        m_cur := max (!m_cur + t.(i)) t.(i);
+        m := max !m !m_cur
+    done;
+    !m;;
+    
